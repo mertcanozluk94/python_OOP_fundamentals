5fe03a3c1802e4e293e7f0d6394071a0765b642c
mertcanozluk94 <35747700+mertcanozluk94@users.noreply.github.com>
2026-04-30 01:16:48 +0300

    Update 02_lab.py

--- OOP/02_lab.py
+++ OOP/02_lab.py
@@ -2,47 +2,47 @@
 from pyarrow import show_info
 
 
-# class Human:
-#     def __init__(self, full_name:str , weight: int | float , height: int | float):
-#         self.full_name = full_name
-#         self.weight = weight
-#         self.height = height
-#     def show_info(self):
-#         return self.__dict__
-# class FootSoldier(Human):
-#     pass
-# class Knight(Human):
-#     pass
-# fs_1 = FootSoldier(full_name="burak", weight=100, height=200)
-# print(fs_1.show_info())
+class Human:
+     def __init__(self, full_name:str , weight: int | float , height: int | float):
+         self.full_name = full_name
+         self.weight = weight
+         self.height = height
+     def show_info(self):
+         return self.__dict__
+ class FootSoldier(Human):
+     pass
+ class Knight(Human):
+     pass
+ fs_1 = FootSoldier(full_name="burak", weight=100, height=200)
+ print(fs_1.show_info())
 
 
-##########
 
 
-# class BaseEntity:
-#     def __init__(self, full_name:str , desc:str):
-#         self.full_name = full_name
-#         self.desc = desc
-#     def show_info(self):
-#         print(
-#             f'name {self.full_name}'
-#             f'description{self.desc})'
-#         )
-# class Category(BaseEntity): #we can add multi class.
-#     pass
-# class Product(BaseEntity):
-#     def __init__(self,full_name,desc,price: float,stock : int):
-#         super().__init__(full_name,desc) # adding main class utilities - super
-#         self.price = price
-#         self.stock = stock
-# p1 = Product(full_name= "gloves",desc='boxing',price=44,stock=3)
-# print(p1.__dict__)
-#     def show_info(self):
-#         super()show.info()
-#         print(
-#             f'Price: {self.price}, stock: {self.stock}'
-#         )
+
+class BaseEntity:
+     def __init__(self, full_name:str , desc:str):
+         self.full_name = full_name
+         self.desc = desc
+     def show_info(self):
+         print(
+             f'name {self.full_name}'
+             f'description{self.desc})'
+         )
+ class Category(BaseEntity): we can add multi class.
+     pass
+ class Product(BaseEntity):
+     def __init__(self,full_name,desc,price: float,stock : int):
+         super().__init__(full_name,desc)  adding main class utilities - super
+         self.price = price
+         self.stock = stock
+ p1 = Product(full_name= "gloves",desc='boxing',price=44,stock=3)
+ print(p1.__dict__)
+     def show_info(self):
+         super()show.info()
+         print(
+             f'Price: {self.price}, stock: {self.stock}'
+         )
 
 class BasePhone:
     def __init__(self,phone_id: str,model: str,brand: str,price:float):
